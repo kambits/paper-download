876294ef9d7712a306aeee11cde69ce3f24bd39a
kambits <kambits.dev@gmail.com>
2021-04-05 00:00:50 +0800

    update

--- paper download/menu.js
+++ paper download/menu.js
@@ -1,29 +1,47 @@
-var link = chrome.contextMenus.create({"title": "paper download","contexts":["link"], "onclick":genericOnClick }); 
+chrome.contextMenus.create({title: "paper download",contexts:["link"], onclick:genericOnClick }); 
 
 function genericOnClick(info, tab) { 
 	var a = info.linkUrl;
-	if (a.substr(0,5) == "http:")
+	console.log(a)
+	if (a.substr(0,6) == "https:")
 	{
-		var name = info.selectionText + ".pdf";
-		var index = a.indexOf("/", 7)
-		var url = (a.substr(0, index) + ".sci-hub.cc" + a.substr(index));
+		
+		// console.log(name)
+		var index = a.indexOf("/", 8)
+		// var url = (a.substr(0, index) + ".sci-hub.se" + a.substr(index));
+		var url = ("https://sci-hub.se/" + a);
+		console.log(url)
 		var xhr = new XMLHttpRequest();
 		xhr.open("GET", url, true);
 		xhr.send(null);	
 		xhr.responseType = "document";
+		// console.log(xhr)
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState == 4) {
 				if (xhr.status==200)
-			    {
-			    var docu = xhr.responseXML;
-			    var div = docu.getElementById("content");
-			    var message =div.getElementsByTagName("iframe");
-			    nodemap = message[0].attributes;
-			    nodeurl = nodemap[0].nodeValue;
-			    chrome.downloads.download({
-				  url: nodeurl,
-				  filename:name
-				});
+			    {	
+					var name = info.selectionText + ".pdf";
+					var docu = xhr.responseXML;
+					var div = docu.getElementById("article");
+					var message =div.getElementsByTagName("iframe");
+					// console.log(message)
+					nodemap = message[0].attributes;
+					console.log(nodemap)
+					nodeurl = nodemap[0].nodeValue;
+					if (nodeurl.substr(0,6) != "https:") {
+						var nodeurl = "https:" + nodeurl
+					}
+					console.log(nodeurl)
+					console.log(name);
+					console.log(typeof(name))
+					var name = String(name).replace("/", "-");
+					console.log(name);
+					var name = String(name).replace(":", "-");
+					console.log(name);
+					chrome.downloads.download({
+					url: nodeurl,
+					filename:name
+					});
 			    }
 			  else
 			    {
